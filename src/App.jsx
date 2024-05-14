9d6a58657b48dc96bb370bb7c7a6a843d82db278
shashikant-wagh <shashikantwagh721@gmail.com>
2024-05-14 13:59:28 +0100

    Updated App.jsx to use chat.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,63 @@
+import React from "react";
+import { useRef, useState, useEffect } from "react";
+
+import chat from "./assets/js/chat.js";
+
+const formatDateTime = (datetime) => {
+  const date = new Date(datetime);
+
+  return (
+    date.toDateString() +
+    ", " +
+    date.toLocaleTimeString("en-US", {
+      hour: "numeric",
+      minute: "2-digit",
+    })
+  );
+};
+
 function App() {
+  const [message, setMessage] = useState("");
+  const [chatHistory, setChatHistory] = useState([]);
+  const messagesEndRef = useRef(null);
+
+  useEffect(() => {
+    chat.getChatHistory(setChatHistory);
+  }, []);
+
+  useEffect(() => {
+    chat.addListener("chatreceived", function (data) {
+      setChatHistory((prev) => [...prev, data.chat]);
+    });
+
+    return () => {
+      chat.removeListener("chatreceived");
+    };
+  }, []);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [chatHistory]);
+
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {
+      // messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    chat.sendChat(message);
+    setMessage("");
+  };
+
   return (
     <div className="flex-1 p:2 sm:p-6 justify-between flex flex-col h-screen">
       <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
         <div className="relative flex items-center space-x-4">
           <div className="flex flex-col leading-tight">
             <div className="text-2xl mt-1 flex items-center">
-              <span className="text-gray-700 mr-3">Chat</span>
+              <span className="text-gray-700 mr-3">React Chat</span>
             </div>
           </div>
         </div>
@@ -14,40 +66,63 @@ function App() {
         id="messages"
         className="flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
       >
-        <div className="chat-message">
-          <div className="flex items-end">
-            <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
-              <div>
-                <span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">
-                  Can be verified on any platform using docker
-                </span>
+        {chatHistory.map((chat, index) => {
+          const positionClass =
+            chat.from.toLowerCase() === "visitor" ? "justify-end" : "";
+
+          const colorClass =
+            chat.from.toLowerCase() === "visitor"
+              ? "bg-blue-600 text-white"
+              : "bg-gray-300 text-gray-600";
+
+          const chatContinues =
+            chat.from.toLowerCase() ===
+            chatHistory[index + 1]?.from?.toLowerCase();
+
+          const shapeClass = chatContinues
+            ? ""
+            : chat.from.toLowerCase() === "visitor"
+            ? "rounded-br-none"
+            : "rounded-bl-none";
+
+          return (
+            <div key={index} className="chat-message">
+              <div className={`flex items-end ${positionClass}`}>
+                <div className="flex flex-col space-y-2 max-w-xs mx-2 order-2 items-start">
+                  <div>
+                    <span
+                      className={`px-4 py-2 rounded-lg inline-block ${shapeClass} ${colorClass}`}
+                    >
+                      {chat.message}
+                    </span>
+                  </div>
+                </div>
               </div>
+
+              {chatContinues ? null : (
+                <div
+                  className={`flex items-end  text-xs text-right mt-2 ${positionClass}`}
+                >
+                  {formatDateTime(chat.datetime)}
+                </div>
+              )}
             </div>
-          </div>
-        </div>
-        <div className="chat-message">
-          <div className="flex items-end justify-end">
-            <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
-              <div>
-                <span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">
-                  Your error message says permission denied, npm global installs
-                  must be given root privileges.
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
+          );
+        })}
+        <div ref={messagesEndRef} />
       </div>
       <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
-        <div className="relative flex">
+        <form className="relative flex" onSubmit={onSubmit}>
           <input
             type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
             placeholder="Write your message!"
-            className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-md py-3"
+            className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-6 bg-gray-200 rounded-md py-3"
           />
           <div className="absolute right-0 items-center inset-y-0 hidden sm:flex">
             <button
-              type="button"
+              type="submit"
               className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none"
             >
               <span className="font-bold">Send</span>
@@ -61,7 +136,7 @@ function App() {
               </svg>
             </button>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
